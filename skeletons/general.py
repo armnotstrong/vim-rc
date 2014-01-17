f29324fe7e4889f5c873837e7599ff96cedde2a9
ximing <ximing@ximing.mb>
2014-01-18 01:37:03 +0800

    first commit

---
+++ skeletons/general.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Author:     Zhao Ximing <ximing.zhao@cloudacc-inc.com>
+# Maintainer: Zhao Ximing <ximing.zhao@cloudacc-inc.com>
+
+<+CURSOR+>
+
+# vim:ai:et:sts=4:sw=4:
